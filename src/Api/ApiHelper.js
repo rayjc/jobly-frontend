3a5f2c380645282112bfd803c962f8cb462e8e82
Ray Chung <ray.j.chung@gmail.com>
2020-07-01 00:22:05 -0700

    Fix env variable.

--- src/Api/ApiHelper.js
+++ src/Api/ApiHelper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { authenticate } from '../util/helpers';
 
-const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3001';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001';
 
 class BaseApi {
   static async request(endpoint, paramsOrData = {}, verb = "get") {
